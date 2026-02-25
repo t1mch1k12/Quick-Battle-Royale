d8b0d86f1b9f1d546bd8f676120cce7dbc4b4e00
t1mch1k12 <kim228455@gmail.com>
2026-02-25 18:52:44 +0300

    cards.sql

--- teams/blue/cards.sql
+++ teams/blue/cards.sql
@@ -1,1 +1,16 @@
+CREATE TABLE IF NOT EXISTS characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL, -- Тип персонажа (например, воин, заклинание, здание)
+    rarity VARCHAR(50) NOT NULL, -- Редкость (обычный, редкий, эпический, легендарный)
+    elixir_cost INT NOT NULL, -- Стоимость эликсира
+    description TEXT, -- Описание персонажа
+    level INT NOT NULL DEFAULT 1, -- Уровень персонажа
+    max_level INT NOT NULL, -- Максимальный уровень персонажа
+    hitpoints INT NOT NULL, -- Здоровье персонажа         
+    damage INT NOT NULL, -- Урон персонажа
+    speed INT NOT NULL, -- Скорость персонажа
+    range INT NOT NULL, -- Дальность атаки
+    target VARCHAR(50) NOT NULL -- Цель атаки (наземные, воздушные, наземные и воздушные)
+);
 
